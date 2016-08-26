2bba0b762f84f5fa7875c48362555555876dfb09
Karl Pokus <karl.pokus@gmail.com>
2016-08-26 13:41:58 +0000

    add -r flag to cp

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # build node
-cp -v src/* public/ && \
+cp -vr src/* public/ && \
 # build chrome
-cp -v src/* chrome/ && \
+cp -vr src/* chrome/ && \
 # zip chrome
 zip -r ext.zip chrome
