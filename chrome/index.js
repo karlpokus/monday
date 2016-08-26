851ee9a84ec5147ac814f80ab7655a48581fb766
Karl Pokus <karl.pokus@gmail.com>
2016-08-26 13:45:06 +0000

    complete rewrite. See release notes

--- chrome/index.js
+++ chrome/index.js
@@ -1,59 +1,222 @@
 var list = {
   el: '.list',
+  blurInput: function(el) {
+    $(el).blur();
+  },
   newItem: function(o) {
     return $('<input/>')
       .prop({type: 'text', value: o.body})
       .data('id', o.id);
   },
-  newId: function() {
+  empty: function() {
+    $(list.el).empty();
+  },
+  remove: function(el) {
+    $(el).remove();
+  },
+  addOne: function(o) {
+    var el = list.newItem(o);
+    $(list.el).prepend(el);
+  },
+  addAll: function(data) {
+    if (data.length > 0) {
+      data.forEach(function(o){
+        var el = list.newItem(o);
+        $(list.el).prepend(el);
+      });
+    }
+  }
+};
+
+var main = {
+  el: '#main',
+  focus: function() {
+    $(main.el).focus();
+  },
+  reset: function() {
+    $(main.el).val('');
+  },
+  historyIndex: 0,
+  showHistory: function(arrow, data) {
+    if (arrow === 'up' && data && main.historyIndex < data.length) {
+      main.render(data.reverse()[main.historyIndex++].body);
+    }
+    if (arrow === 'down' && data && main.historyIndex > 0) {
+      main.render(data.reverse()[--main.historyIndex].body);
+    }
+  },
+  render: function(str) {
+    $(main.el).val(str);
+  }
+};
+
+var tag = {
+  el: '.tag',
+  render: function(str) {
+    $(tag.el).text(str);
+  }
+};
+
+var app = {
+  v: 'v2.0',
+  init: function() {
+    app.setContext(function(){
+      app.addItems();
+      main.focus();
+    });
+  },
+  setContext: function(cb) {
+    if (typeof chrome === 'object' && typeof chrome.storage === 'object') {
+      db = chrome_store;
+      app.ctx = 'chrome ext';
+    } else {
+      db = localStorage_store;
+      app.ctx = 'browser';
+    }
+    cb();
+  },
+  createId: function() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
       return v.toString(16);
     });
   },
-  add: function(o) {
-    var el = this.newItem(o);
-    $(this.el).prepend(el);
+  addItems: function() {
+    db.fetch(list.addAll);
   },
-  remove: function(el) {
-    $(el).remove();
+  addItem: function(body) {
+    var o = {
+      body: body,
+      id: app.createId()
+    };
+    db.add(o, list.addOne);
+  },
+  removeItem: function(el, id) {
+    db.remove(el, id, list.remove);
+  },
+  updateItem: function(el, body, id) {
+    db.update(el, body, id, list.blurInput);
+  },
+  history: function(arrow) {
+    db.fetch(function(data){
+      main.showHistory(arrow, data);
+    });
+  },
+  filter: function(filterString) {
+    list.empty();
+    tag.render(filterString);
+    main.reset();
+    db.fetch(function(data){
+      if (filterString && data.length > 0) {
+        data = data.filter(function(o){
+          return new RegExp(filterString.substr(1), 'i').test(o.body);
+        });
+      }
+      list.addAll(data);
+    });
+  },
+  cmd_url: function(body, cb) {
+    var url;
+    if (this.ctx === 'chrome ext') {
+      chrome.tabs.query({'active':true, 'lastFocusedWindow':true}, function(tabs) {
+        url = tabs[0].url;
+        cb(url);
+      });
+    } else { // browser
+      url = window.location.href;
+      cb(url);
+    }
+    main.reset();
+  },
+  cmd_export: function() {
+    db.fetch(function(data){
+      if (data.length > 0) {
+        data = data.map(function(o){
+          return o.body + '\n';
+        }).reverse();
+        var d = new Date(),
+            str = d.toJSON(),
+            header = 'Monday ' + app.v + ' Export ' + str + '\n';
+        data.unshift(header);
+        data = data.join('');
+        window.prompt("Copy to clipboard", data);
+        main.reset();
+      }
+    });
+  },
+  cmd_drop: function() {
+    var flag = window.confirm("Delete all data?");
+    if (flag) {
+      db.drop(function(){
+        list.empty();
+        main.reset();
+        main.focus();
+      });
+    } else {
+      main.reset();
+    }
   }
 };
 
 // init
 $(function(){
-  db.all();
-  $('#main').focus();
+  app.init();
 
-  // EVENT - add sticky
+  // EVENT - events on main input
   $('#main').on('keyup', function(e){
+    if (e.which === 38) {
+      app.history('up');
+      return;
+    }
+    if (e.which === 40) {
+      app.history('down');
+      return;
+    }
     if (e.which === 13) {
+      main.historyIndex = 0;
       var body = $(this).val().trim();
-      if (body) {
-        var o = {
-          body: body,
-          id: list.newId()
-        };
-        list.add(o);
-        db.add(o);
+
+      if (/^\$url$/.test(body)) {
+        app.cmd_url(body, function(data){
+          app.addItem(data);
+        });
+        return;
       }
-      $(this).val('');
+      if (/^\$export$/.test(body)) {
+        app.cmd_export();
+        return;
+      }
+      if (/^\$drop$/.test(body)) {
+        app.cmd_drop();
+        return;
+      }
+      if (/^#.+/.test(body) && !/\s/.test(body)) {
+        app.filter(body);
+        return;
+      }
+      if (/^!#$/.test(body)) {
+        app.filter('');
+        return;
+      }
+      if (body) {
+        app.addItem(body);
+        main.reset();
+      }
     }
   });
 
-  // EVENT - edit sticky
+  // EVENT - edit item in list
   $('.list').on('keyup', 'input', function(e){
     if (e.which === 13) {
       var body = $(this).val().trim(),
           id = $(this).data('id');
+
       if (body.toLowerCase() === 'done' || !body) {
-        list.remove(this);
-        db.remove(id);
+        app.removeItem(this, id);
       } else {
-        db.update(body, id);
-        $(this).blur();
+        app.updateItem(this, body, id);
       }
-      $('#main').focus();
+      main.focus();
     }
   });
 });
--- src/db.js
+++ src/db.js
@@ -1,93 +1,91 @@
-var localStorage_store = { // [{id, body}, {}..]
-  fetch: function() {
-    var str = localStorage.getItem("monday_items");
-    return (str) ? JSON.parse(str) : str;
-  },
-  save: function(data) {
-    localStorage.setItem("monday_items", JSON.stringify(data));
-  },
-  all: function() {
-    var data = this.fetch();
-    if (data && data.length > 0) {
-      data.forEach(function(o){
-        list.add(o);
-      });
-    } else if (!data) {
-      this.save([]); // init
-    }
-  },
-  add: function(o) {
-    var data = this.fetch();
-    data.push(o);
-    this.save(data);
-  },
-  remove: function(id) {
-    var data = this.fetch();
-    data = data.filter(function(o){
-      return o.id !== id;
-    });
-    this.save(data);
-  },
-  update: function(body, id) {
-    var data = this.fetch();
-    data = data.map(function(o){
-      if (o.id === id) {o.body = body;}
-      return o;
-    });
-    this.save(data);
-  }
-};
-
-var chrome_store = { // {<id>: <str>, ..}
-  n: 0,
-  badge: function(x) {
-    var n;
-    if (!x) {
-      n = this.n;
-    }
-    if (x === '>') {
-      n = ++this.n;
-    }
-    if (x === '<') {
-      n = --this.n;
-    }
+var chrome_store = {
+  badge: function(n) {
     if (n === 0) {
       n = '';
     }
     chrome.browserAction.setBadgeText({text: n.toString()});
   },
-  all: function() {
-    var self = this;
-    chrome.storage.sync.get(null, function(o) {
-      for (var prop in o) {
-        var item = {id: prop, body: o[prop]};
-        list.add(item);
-      }
-      self.n = Object.keys(o).length;
-      self.badge();
+  fetch: function(cb) {
+    chrome.storage.sync.get({monday_items: []}, function(o) {
+      var data = o.monday_items;
+      db.badge(data.length); // temp hack!
+      cb(data);
     });
   },
-  add: function(o) {
-    var item = {};
-    item[o.id] = o.body;
-    chrome.storage.sync.set(item);
-    this.badge('>');
+  save: function(data) {
+    chrome.storage.sync.set({monday_items: data}, function(){
+      db.badge(data.length);
+    });
   },
-  remove: function(id) {
-    chrome.storage.sync.remove(id);
-    this.badge('<');
+  add: function(o, cb) {
+    db.fetch(function(data){
+      data.push(o);
+      db.save(data);
+      cb(o);
+    });
   },
-  update: function(body, id) {
-    var item = {};
-    item[id] = body;
-    chrome.storage.sync.set(item);
+  remove: function(el, id, cb) {
+    db.fetch(function(data){
+      data = data.filter(function(o){
+        return o.id !== id;
+      });
+      db.save(data);
+      cb(el);
+    });
+  },
+  update: function(el, body, id, cb) {
+    db.fetch(function(data){
+      data = data.map(function(o){
+        if (o.id === id) {o.body = body;}
+        return o;
+      });
+      db.save(data);
+      cb(el);
+    });
+  },
+  drop: function(cb) {
+    db.save([]);
+    cb();
   }
 };
 
-var db;
-
-if (typeof chrome === 'object' && typeof chrome.storage === 'object') {
-  db = chrome_store;
-} else {
-  db = localStorage_store;
-}
+var localStorage_store = {
+  fetch: function(cb) {
+    var str = localStorage.getItem("monday_items"),
+        arg = (str) ? JSON.parse(str) : [];
+    cb(arg);
+  },
+  save: function(data) {
+    localStorage.setItem("monday_items", JSON.stringify(data));
+  },
+  add: function(o, cb) {
+    db.fetch(function(data){
+      data.push(o);
+      db.save(data);
+      cb(o);
+    });
+  },
+  remove: function(el, id, cb) {
+    db.fetch(function(data){
+      data = data.filter(function(o){
+        return o.id !== id;
+      });
+      db.save(data);
+      cb(el);
+    });
+  },
+  update: function(el, body, id, cb) {
+    db.fetch(function(data){
+      data = data.map(function(o){
+        if (o.id === id) {o.body = body;}
+        return o;
+      });
+      db.save(data);
+      cb(el);
+    });
+  },
+  drop: function(cb) {
+    db.save([]);
+    cb();
+  }
+};
